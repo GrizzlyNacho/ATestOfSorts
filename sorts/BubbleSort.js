39a8628f9adb2ecead19c9d729c9c805afe8140f
GrizzlyNacho <nlutz91@gmail.com>
2015-08-03 22:06:48 -0400

    added sort algorithm interface

--- sorts/BubbleSort.js
+++ sorts/BubbleSort.js
@@ -1,37 +1,36 @@
 /**
  * Standard Bubble sort.
  * Operates in O(n*n) time.
- * @param {Array} numberSet
+ * @param {Array} startingSet
  * @param {Function} drawFunction
  */
-var BubbleSort = function BubbleSort(numberSet, drawFunction) {
-        this.__maxLength = numberSet.length;
+var BubbleSort = function BubbleSort(startingSet, drawFunction) {
+        SortAlgorithm.call(this, startingSet, drawFunction);
         this.__position = 0;
         this.__iterationContainsChange = 0;
         this.__iterationCount = 0;
-
-        this.__numberSet = numberSet;
-        this.__util = new Util();
-        this.__draw = drawFunction;
-
         this.step();
 };
+
+// Handle Class inheritance
+BubbleSort.prototype = Object.create(SortAlgorithm.prototype);
+BubbleSort.prototype.constructor = BubbleSort;
 
 /**
  * Perform one step of the sort.
  */
 BubbleSort.prototype.step = function step() {
-    if (this.__position >= this.__maxLength) {
+    if (this.__position >= this._maxLength) {
         if (!this.__iterationContainsChange) {
             // We have finished a full iteration without changing any values.
             // We are sorted.
             console.log("Bubble Sort terminated after "
-                + (this.__iterationCount * this.__maxLength + this.__position)
+                + (this.__iterationCount * this._maxLength + this.__position)
                 + " steps.");
             // Draw the last state
-            this.__draw(
+            this._draw(
                 [{
-                    numberSet: this.__numberSet,
+                    numberSet: this._numberSet,
                 }]
             );
             return;
@@ -42,17 +41,17 @@ BubbleSort.prototype.step = function step() {
     }
 
     var changeOccurred = false;
-    if (this.__numberSet[this.__position] > this.__numberSet[this.__position+1]) {
-        this.__util.swap(this.__numberSet, this.__position, this.__position+1);
+    if (this._numberSet[this.__position] > this._numberSet[this.__position+1]) {
+        this._util.swap(this._numberSet, this.__position, this.__position+1);
         this.__iterationContainsChange = true;
         changeOccurred = true;
     }
 
     this.__position += 1;
     if (changeOccurred) {
-        this.__draw(
+        this._draw(
             [{
-                numberSet: this.__numberSet,
+                numberSet: this._numberSet,
                 alteredIndices: [this.__position, this.__position+1],
             }],
             this.step.bind(this)
